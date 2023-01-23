e079dc25ac58a9ca15b4225c80076b0779769ee1
Nil-Andreu <nilandreug@gmail.com>
2023-01-23 22:19:44 +0100

    [NEW] get_db_params

--- postgres_backup/backup.py
+++ postgres_backup/backup.py
@@ -16,5 +16,17 @@ class Backup:
 
         self.db_uri = db_uri
 
+    def get_db_params(
+        self,
+    ):
+        user_pass, host_db = self.db_uri.split('@')
+
+        username, password = user_pass.split(':')[1:]
+        username = username.split('//')[1]
+
+        host, port_database = host_db.split(':')
+        port, db_name = port_database.split('/')
+
+        return username, password, host, port, db_name
 
 
