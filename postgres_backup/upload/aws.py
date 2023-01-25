730434f54322ad3378892e3cea72a157fc5118f9
Nil-Andreu <nilandreug@gmail.com>
2023-01-25 09:50:42 +0100

    [NEW] create_bucket

--- postgres_backup/upload/aws.py
+++ postgres_backup/upload/aws.py
@@ -5,11 +5,13 @@ class AWSStorage:
     def __init__(
         self,
         s3_resource,
-        bucket_name: typing.Optional[str] = 'backup'
+        region_name: typing.Optional[str],
+        bucket_name: typing.Optional[str] = 'backup',
     ):
 
         self.s3_resource = s3_resource
         self.bucket_name = bucket_name
+        self.region_name = region_name
 
     def list_bucket_name(self) -> typing.List[str]:
         """
@@ -18,3 +20,19 @@ class AWSStorage:
         """
 
         return [bucket.name for bucket in self.s3_resource.buckets.all()]
+
+    def create_bucket(self):
+        """
+        In the case we do not have a bucket already created for storing backups, we can create it.
+        :param storage_class: the type of bucket we want to create
+        :return: True if created correctly storage class
+        """
+
+        self.s3_resource.create_bucket(
+            Bucket=self.bucket_name,
+            CreateBucketConfiguration={
+                'LocationConstraint': self.region_name
+            }
+        )
+
+        return True
