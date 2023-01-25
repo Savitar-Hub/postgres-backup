bd1050c58eb49dab2a474970496793ba2e9e5c6a
Nil-Andreu <nilandreug@gmail.com>
2023-01-25 09:36:10 +0100

    [NEW] AWS list_bucket_name

--- postgres_backup/upload/aws.py
+++ postgres_backup/upload/aws.py
@@ -10,3 +10,11 @@ class AWSStorage:
 
         self.client = client
         self.bucket_name = bucket_name
+
+    def list_bucket_name(self) -> typing.List[str]:
+        """
+        List the bucket names that we have.
+        :return: list with all the names of the actual buckets
+        """
+
+        return [bucket.name for bucket in self.client.buckets.all()]
